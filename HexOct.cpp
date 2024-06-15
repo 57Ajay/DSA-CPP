1adfe4f911a4e6c47fadfd0afd0801467db29f18
57Ajay <57ajay.u@gmail.com>
2024-06-15 19:25:25 +0530

    simple code for hex and oct values

---
+++ HexOct.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int x, y;
+    x = 23;
+    y = 90;
+    cout << oct;
+    cout << x << " " << y << endl;
+    cout << hex;
+    cout << x << " " << y << endl;
+    cout << dec;
+    cout << x << " " << y << endl;
+
+    return 0;
+}
--- main.cpp
+++ main.cpp
@@ -2,8 +2,5 @@
 using namespace std;
 
 int main(){
-
-    cout << "Hello World!" << endl;
-
     return 0;
 }
